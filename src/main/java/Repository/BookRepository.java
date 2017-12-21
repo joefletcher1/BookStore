93fb7e83a198e054eb9d1c3daec7d0f4e79b9057
JoeFletcher1 <joe-fletcher@live.co.uk>
2017-12-21 17:25:46 +0000

    module 4 - jPA

---
+++ src/main/java/Repository/BookRepository.java
@@ -0,0 +1,26 @@
+package Repository;
+
+import com.pluralsight.bookstore.model.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class BookRepository {
+
+    @PersistenceContext(unitName = "bookStorePU")
+    private EntityManager em;
+
+    public Book find(Long id) {
+        return em.find(Book.class, id);
+    }
+
+    public Book create(Book book) {
+        em.persist(book);
+        return book;
+    }
+
+    public void delete(Long id) {
+        em.remove(em.getReference(Book.class, id));
+    }
+
+}
